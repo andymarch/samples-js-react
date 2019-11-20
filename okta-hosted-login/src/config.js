de3c501ec0e611f9a3aff45b31ae0819275b1b02
Andy March <mail@andymarch.co.uk>
2019-11-20 12:19:34 +0000

    removing local port reference

--- okta-hosted-login/src/config.js
+++ okta-hosted-login/src/config.js
@@ -6,7 +6,7 @@ export default {
   oidc: {
     clientId: CLIENT_ID,
     issuer: ISSUER,
-    redirectUri: 'http://localhost:8080/implicit/callback',
+    redirectUri: 'https://okta-ie11-pkce.herokuapp.com//implicit/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK,
